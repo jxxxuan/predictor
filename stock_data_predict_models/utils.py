d172839b627953b7dc6f0adbf062d44a75da74ae
jxxxuan <ngjunxuan1120@gmail.com>
2022-07-29 14:25:22 +0800

    29-7-22.1

--- stock_data_predict_models/utils.py
+++ stock_data_predict_models/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pandas as pd
-import tensorflow as tf
-import random
+from tensorflow.data import Dataset
+from random import choice
+from time import time
 
 class DataProcessor():
     def __init__(self,data,batch_size):
@@ -10,32 +10,24 @@ class DataProcessor():
 
     def sample(self):
         x = np.random.randint(self.data.shape[1],size=[self.batch_size],dtype='int16')
-        pos = []
-        neg = []
-        for i in x:
-            pos.append(self.data[self.data[:,i] == 1])
-            neg.append(self.data[self.data[:,i] == -1])
-            pos[-1] = pos[-1][random.randint(0,(pos[-1].shape[0]-1))]
-            neg[-1] = neg[-1][random.randint(0,(neg[-1].shape[0]-1))]
-        pos = np.array(pos)
-        neg = np.array(neg)
-        return x,np.concatenate([pos,neg],axis=1)
+
+        yp = []
+        yn = []
+        for i in range(self.batch_size):
+            yp.append(choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == 1]))  
+            yn.append(choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == -1]))
+
+        return x,np.concatenate([self.data[yp],self.data[yn]],axis=1)
 
     def toDataBatch(self,batchsz=8):
         x,y = self.sample()
-        DataBatch = tf.data.Dataset.from_tensor_slices((x,y))
+        DataBatch = Dataset.from_tensor_slices((x,y))
         DataBatch = DataBatch.batch(batchsz)
         return DataBatch
 
 if __name__ == '__main__':
     train_data = np.load(r'train_data.npy')
-    test_data = np.load(r'test_data.npy')
-    print('read')
-    train_dp = DataProcessor(train_data,4)
-    test_dp = DataProcessor(test_data,4)
-    print('processed')
+    t1 = time()
+    train_dp = DataProcessor(train_data,128)
     train_db = train_dp.toDataBatch()
-    test_db = test_dp.toDataBatch()
-    sample = next(iter(train_db))
-    print('batch:', sample)
-    
+    print(time() - t1)
