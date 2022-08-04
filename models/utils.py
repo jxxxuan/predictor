4e1e4e25dd9b2075396c2e6a5cbf1b73d07df3a0
ng-jun-xuan <ngjunxuan1120@gmail.com>
2022-08-04 23:24:40 +0800

    8-4-22.23:24

--- models/utils.py
+++ models/utils.py
@@ -4,7 +4,7 @@ from numpy import random
 from time import time
 
 class DataProcessor():
-    def __init__(self,data,batch_size):
+    def __init__(self,data,batch_size,avr):
         self.data = data
         self.batch_size = batch_size
 
@@ -14,8 +14,8 @@ class DataProcessor():
         yp = []
         yn = []
         for i in range(self.batch_size):
-            yp.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == 1], size=[10]))  
-            yn.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == -1], size=[10]))
+            yp.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == 1], size=[avr]))  
+            yn.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == -1], size=[avr]))
 
         y = np.concatenate([self.data[yp],self.data[yn]],axis=2)
         y = np.mean(y,axis=1)
