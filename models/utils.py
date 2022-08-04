7e8e28a0eab74e3b717a434bb3d3e10e7a355d62
ng-jun-xuan <ngjunxuan1120@gmail.com>
2022-08-04 23:29:11 +0800

    8-4-22.23:29

--- models/utils.py
+++ models/utils.py
@@ -7,6 +7,7 @@ class DataProcessor():
     def __init__(self,data,batch_size,avr):
         self.data = data
         self.batch_size = batch_size
+        self.avr= avr
 
     def sample(self):
         x = np.random.randint(self.data.shape[1],size=[self.batch_size],dtype='int16')
@@ -14,8 +15,8 @@ class DataProcessor():
         yp = []
         yn = []
         for i in range(self.batch_size):
-            yp.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == 1], size=[avr]))  
-            yn.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == -1], size=[avr]))
+            yp.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == 1], size=[self.avr]))  
+            yn.append(random.choice(np.arange(self.data.shape[0])[self.data[:,x[i]] == -1], size=[self.avr]))
 
         y = np.concatenate([self.data[yp],self.data[yn]],axis=2)
         y = np.mean(y,axis=1)
