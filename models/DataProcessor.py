20a83f5361d752947a07616d09d2e354ac221cd8
jxxxuan <87358766+jxxxuan@users.noreply.github.com>
2022-10-23 12:35:48 +0800

    2022-10-23 12:35:39

--- models/DataProcessor.py
+++ models/DataProcessor.py
@@ -6,11 +6,11 @@ from tensorflow.data import Dataset
 import random
 from numpy import random as nrandom
 from time import time
-
+'''
 import sys
 sys.path.append(r'D:\Documents\predictor\news')
 from news_processor import utils
-
+'''
 
 class Cdata6808Processor():
     def __init__(self,data,batch_size,avr=1):
@@ -121,7 +121,7 @@ class NewsProcessor():
         self.max_length = max_length
 
     def load_data(self,file_path):
-        '''
+        
         with open(file_path,'r') as reader:
             data = json.loads(reader.read())
         '''
@@ -134,7 +134,7 @@ class NewsProcessor():
                 for p in news['content']:
                     data.append(tuple(p))
         return data
-
+'''
     def load_vocab_file(self,vocab_file):
         vocab = pd.read_csv(vocab_file,index_col=0)
         return vocab[vocab['en_Trainable']]['en_ids'].to_dict()
